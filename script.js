d80b01a8298824fb6fe8bf747f7a6b4f25b0551a
naiden <na4ko359@gmail.com>
2022-01-01 19:08:28 +0200

    updated js/menu navigation

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.getElementById("hamburger").addEventListener("click", () => {
     document.getElementById("nav-ul").classList.toggle("show");
+    if (document.getElementById("menu_icon").getAttribute("src") === "images/icon-hamburger.svg") {
+        document.getElementById("menu_icon").setAttribute("src", "images/close_black_36dp.svg");
+    } else {
+        document.getElementById("menu_icon").setAttribute("src", "images/icon-hamburger.svg");
+    };
 });
